af53935cf9bfe8cbd97c8397422f0cba51f9c52d
Maria Fernanda Montezuma  Gonzalez <maria.montezuma@globant.com>
2025-07-21 15:33:41 -0500

    feat: se cfrea programa para ingresar y editar datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+            Scanner scanner=new Scanner(System.in);
+            String nombre, especialidad, biografia;
+
+        System.out.print("Ingrese su nombre: ");
+            nombre= scanner.nextLine();
+        System.out.print("Ingrese su especialidad: ");
+            especialidad=scanner.nextLine();
+        System.out.print("Ingrese su biografia: ");
+            biografia=scanner.nextLine();
+
+        System.out.println("El profesional \u001B[32m" + nombre + "\u001B[0m especializado en \u001B[32m" + especialidad + "\u001B[0m\n\u001B[32m" + biografia + "\u001B[0m");
+        
     }
+
 }
